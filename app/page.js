a97fba7e0ae2454226a119058a1b997aadaa8d9f
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-03-12 15:21:44 +0200

    version 1.0

--- app/page.js
+++ app/page.js
@@ -4,35 +4,29 @@ import { useModalStore } from "./store/modal";
 import OrderModal from "./components/OrderModal";
 
 const categories = {
-  "Кепки": [
-    { id: 1, name: "Кепка чорна", price: "500 грн", img: "https://via.placeholder.com/150" },
-    { id: 2, name: "Кепка біла", price: "550 грн", img: "https://via.placeholder.com/150" },
-    { id: 3, name: "Кепка червона", price: "600 грн", img: "https://via.placeholder.com/150" },
+  "Блокнот": [
+    { id: 1, name: "Блокнот чорний", price: "500 грн", img: "./img/8.png" },
+    { id: 2, name: "Блокнот білий", price: "550 грн", img: "./img/7.png" },
+    { id: 3, name: "Блокнот білий", price: "550 грн", img: "./img/7.png" },
   ],
   "Футболки": [
-    { id: 4, name: "Футболка чорна", price: "700 грн", img: "https://via.placeholder.com/150" },
-    { id: 5, name: "Футболка біла", price: "750 грн", img: "https://via.placeholder.com/150" },
-    { id: 6, name: "Футболка з принтом", price: "800 грн", img: "https://via.placeholder.com/150" },
+    { id: 5, name: "Худi", price: "750 грн", img: "/img/hudi.jpg" },
+    { id: 6, name: "Футболка чорна", price: "800 грн", img: "/img/6.png" },
   ],
   "Чашки": [
-    { id: 7, name: "Чашка з логотипом", price: "250 грн", img: "https://via.placeholder.com/150" },
-    { id: 8, name: "Чашка біла", price: "200 грн", img: "https://via.placeholder.com/150" },
-    { id: 9, name: "Чашка чорна", price: "300 грн", img: "https://via.placeholder.com/150" },
+    { id: 7, name: "Чашка з логотипом", price: "250 грн", img: "./img/17.jpg" },
+    { id: 8, name: "Чашка біла", price: "200 грн", img: "./img/1.png" },
+    { id: 9, name: "Чашка чорна", price: "300 грн", img: "./img/2.png" },
   ],
-  "Худі": [
-    { id: 10, name: "Худі чорне", price: "1200 грн", img: "https://via.placeholder.com/150" },
-    { id: 11, name: "Худі біле", price: "1250 грн", img: "https://via.placeholder.com/150" },
-    { id: 12, name: "Худі з принтом", price: "1300 грн", img: "https://via.placeholder.com/150" },
-  ],
-  "Браслети з паракорду": [
-    { id: 13, name: "Браслет чорний", price: "300 грн", img: "https://via.placeholder.com/150" },
-    { id: 14, name: "Браслет зелений", price: "320 грн", img: "https://via.placeholder.com/150" },
-    { id: 15, name: "Браслет камуфляж", price: "350 грн", img: "https://via.placeholder.com/150" },
+  "Шопери": [
+    { id: 10, name: "Шопер білий", price: "1200 грн", img: "./img/3.png" },
+    { id: 11, name: "Шопер чорний", price: "1250 грн", img: "./img/4.png" },
+    { id: 12, name: "Шопер чорний", price: "1250 грн", img: "./img/4.png" },
   ],
   "Сувеніри-головоломки": [
-    { id: 16, name: "Кубик-головоломка", price: "400 грн", img: "https://via.placeholder.com/150" },
-    { id: 17, name: "Змійка-головоломка", price: "450 грн", img: "https://via.placeholder.com/150" },
-    { id: 18, name: "Гексагон-головоломка", price: "500 грн", img: "https://via.placeholder.com/150" },
+    { id: 15, name: "Кубик-головоломка", price: "400 грн", img: "./img/15.jpg" },
+    { id: 16, name: "Змійка-головоломка", price: "450 грн", img: "./img/14.jpg" },
+    { id: 17, name: "Гексагон-головоломка", price: "500 грн", img: "./img/19.jpg" },
   ],
 };
 
@@ -50,7 +44,7 @@ export default function Home() {
             {products.map((product) => (
               <div key={product.id} className="bg-white p-4 rounded-lg shadow">
                 <h3 className="text-lg font-bold">{product.name}</h3>
-                <img src={product?.img} alt={product?.name} className="w-full h-40 object-cover rounded my-2" />
+                <img src={product?.img} alt={product?.name} className="w-full h-130 object-cover rounded my-2" />
                 <p className="text-gray-600">{product.price}</p>
                 <button
                   onClick={() => openModal(product)}
